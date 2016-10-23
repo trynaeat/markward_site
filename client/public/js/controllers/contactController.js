56824d9784bf844d5c91527f37ef9dd2f00034a1
jakparks <jakparks@cisco.com>
2016-10-23 16:03:35 -0400

    Added email post alerts.

--- client/public/js/controllers/contactController.js
+++ client/public/js/controllers/contactController.js
@@ -1,7 +1,7 @@
 var ms = angular.module('marksite');
 ms.controller('contactController', ['$scope', '$http', '$window', function($scope, $http, $window) {
 
-  $scope.alerts = [];
+  $scope.alert = null;
 
   $scope.closeAlert = function(index) {
     $scope.alerts.splice(index, 1);
@@ -10,17 +10,20 @@ ms.controller('contactController', ['$scope', '$http', '$window', function($scop
   $scope.emailForm = {};
   $scope.submitForm = function() {
     $http.post('/api/email', $scope.emailForm).then(function(data) {
-      $scope.alerts.push({
+      $scope.alert = {
         type: 'success',
         msg: 'Email sent successfully!'
-      });
-      $window.location.reload();
+      };
     })
     .catch(function() {
-      $scope.alerts.push({
+      $scope.alert = {
         type: 'danger',
         msg: 'Email send failed! Try again later.'
-      });
+      };
     });
+  }
+
+  $scope.closeAlert = function() {
+    $scope.alert = null;
   }
 }]);
